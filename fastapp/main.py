50b4650f2fb90a6a3ed68b1c156d7aa5b81f1f82
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-04-02 11:41:22 +1100

    adding more help into to the cli and putting in docs

--- fastapp/main.py
+++ fastapp/main.py
@@ -8,20 +8,42 @@ app = typer.Typer()
 
 @app.command()
 def cookiecutter(
-    template: str = "",
-    checkout: str = None,
-    no_input: bool = False,
-    extra_context: str = None,
-    replay: bool = False,
-    overwrite_if_exists: bool = False,
-    output_dir: Path = ".",
-    config_file: Path = None,
-    default_config: bool = False,
-    directory: str = None,
-    skip_if_file_exists: bool = False,
+    template: str = typer.Option(
+        "",
+        help="The Cookiecutter template to use. "
+        "If left blank then it uses the default template in this version of fastapp. "
+        "If 'gh' or 'github' then it uses the latest fastapp cookiecutter template at https://github.com/rbturnbull/fastapp-cookiecutter",
+    ),
+    checkout: str = typer.Option(
+        None, help="A branch, tag or commit to checkout after git clone"
+    ),
+    no_input: bool = typer.Option(
+        False, help="Do not prompt for parameters and only use cookiecutter.json file content"
+    ),
+    extra_context: str = typer.Option(None, help=""),
+    replay: bool = typer.Option(
+        False, help="Do not prompt for parameters and only use information entered previously"
+    ),
+    overwrite_if_exists: bool = typer.Option(
+        False, help="Overwrite the contents of the output directory if it already exists"
+    ),
+    output_dir: Path = typer.Option(
+        ".", help="Where to output the generated project dir into"
+    ),
+    config_file: Path = typer.Option(None, help="User configuration file"),
+    default_config: bool = typer.Option(
+        False, help="Do not load a config file. Use the defaults instead"
+    ),
+    directory: str = typer.Option(
+        None,
+        help="Directory within repo that holds cookiecutter.json file for advanced repositories with multi templates in it",
+    ),
+    skip_if_file_exists: bool = typer.Option(
+        False, help="Skip the files in the corresponding directories if they already exist"
+    ),
 ):
     """
-    Generate a fastapp project using cookiecutter.
+    Generate a fastapp project using Cookiecutter (https://github.com/audreyr/cookiecutter)
     """
     if not template:
         template = Path(__file__).parent.resolve() / "cookiecutter"
