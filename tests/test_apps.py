a25e2671de15a111e492c51f295f697ef2f3d172
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-04-15 00:37:47 +1000

    simplifying test

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -6,5 +6,4 @@ def test_model_defaults_change():
         def model(self, size: int = fa.Param(default=2)):
             assert size == 2
 
-    app = DummyApp()
-    app.model()
+    DummyApp().model()
