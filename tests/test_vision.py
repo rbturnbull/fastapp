e5b3786175342ce1b85b355eba4fed1fb6379274
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-04-16 23:10:30 +1000

    adding test for dodgy model name

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -1,6 +1,12 @@
+import pytest
 from fastapp.testing import FastAppTestCase
 from fastapp.vision import VisionApp
 
 
 class TestVisionApp(FastAppTestCase):
     app_class = VisionApp
+
+    def test_model_incorrect(self):
+        app = self.get_app()
+        with pytest.raises(ValueError):
+            app.model(model_name="resnet1000")
