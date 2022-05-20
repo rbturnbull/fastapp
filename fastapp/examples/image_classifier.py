0780d9715efc5c5a23aef5316bbcfdff90d1809d
Ubuntu <ubuntu@herbarium.novalocal>
2022-05-20 01:30:20 +0000

    :zap: getting image classifier to work

--- fastapp/examples/image_classifier.py
+++ fastapp/examples/image_classifier.py
@@ -3,10 +3,11 @@ from fastai.data.block import DataBlock, CategoryBlock
 from fastai.data.transforms import ColReader, RandomSplitter, DisplayedTransform
 from fastai.metrics import accuracy
 from fastai.vision.data import ImageBlock
+from fastai.vision.augment import Resize, ResizeMethod
 import pandas as pd
 import fastapp as fa
-from fastapp.vision import VisionApp
 
+from fastapp.vision import VisionApp
 
 class PathColReader(DisplayedTransform):
     def __init__(self, column_name: str, base_dir: Path):
@@ -39,12 +40,16 @@ class ImageClassifier(VisionApp):
             default=0.2, help="The proportion of the dataset to keep for validation"
         ),
         batch_size: int = fa.Param(default=16, help="The number of items to use in each batch."),
+        width: int = fa.Param(default=224, help="The width to resize all the images to."),
+        height: int = fa.Param(default=224, help="The height to resize all the images to."),
+        resize_method:str = fa.Param(default="squish", help="The method to resize images."),
     ):
         datablock = DataBlock(
             blocks=[ImageBlock, CategoryBlock],
             get_x=PathColReader(column_name=image_column, base_dir=base_dir),
             get_y=ColReader(category_column),
             splitter=RandomSplitter(validation_proportion),
+            item_tfms=Resize( (width,height), method=resize_method ),
         )
         df = pd.read_csv(csv)
 
@@ -52,6 +57,9 @@ class ImageClassifier(VisionApp):
 
     def metrics(self):
         return [accuracy]
+
+    def monitor(self):
+        return "accuracy"
 
 
 if __name__ == "__main__":
