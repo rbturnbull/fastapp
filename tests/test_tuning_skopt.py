7c0d571fe2e05568274c64f83e94ffd593ae892c
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-06-04 16:06:34 +1000

    :cop: adding tests for skopt

--- tests/test_tuning_skopt.py
+++ tests/test_tuning_skopt.py
@@ -1,35 +1,8 @@
-from optuna import samplers
 import pytest
-import numpy as np
 from numpy.testing import assert_allclose
 from .tuning_test_app import TuningTestApp
-
-
-# def test_skopt_tune_default():
-#     app = TuningTestApp()
-#     runs = 120
-#     result = app.tune(engine="skopt", runs=runs, seed=42)
-#     assert len(result.trials) == runs
-#     assert result.best_value > 9.93
-#     assert result.best_trial.number == 104
-#     assert isinstance(result.sampler, samplers.RandomSampler)
-#     df = result.trials_dataframe()
-#     assert "params_a" in df.columns
-#     assert "params_x" in df.columns
-#     assert "params_string" in df.columns
-
-
-# def test_skopt_tune_cmaes():
-#     app = TuningTestApp()
-#     result = app.tune(engine="skopt", method="cmaes", runs=15, seed=42, string="abcdefghij")
-#     assert len(result.trials) == 15
-#     assert result.best_value > 9.8
-#     assert result.best_trial.number == 6
-#     assert isinstance(result.sampler, samplers.CmaEsSampler)
-#     df = result.trials_dataframe()
-#     assert "params_a" in df.columns
-#     assert "params_x" in df.columns
-#     assert "params_string" not in df.columns
+import skopt
+from fastapp.tuning.skopt import get_optimizer
 
 
 def test_skopt_tune_random():
@@ -49,10 +22,71 @@ def test_skopt_tune_random():
             assert found == desired
 
 
-def test_get_optimizer():
-    import skopt
-    from fastapp.tuning.skopt import get_optimizer
+def test_skopt_tune_bayes():
+    app = TuningTestApp()
+    runs = 30
+    result = app.tune(engine="skopt", method="bayes", runs=runs, seed=42)
+    assert len(result.func_vals) == runs
+    assert result.fun < -9.97
+    assert result.space.n_dims == 3
+    assert type(result.space[0][1]).__name__ == 'Real'
+    assert type(result.space[1][1]).__name__ == 'Integer'
+    assert type(result.space[2][1]).__name__ == 'Categorical'
+    for found, desired in zip(result.x, [1.9370031589297412, 6, 'abcdefghij']):
+        if isinstance(found, float):
+            assert_allclose(found, desired)
+        else:
+            assert found == desired
 
+
+def test_skopt_tune_bayes_2param():
+    app = TuningTestApp()
+    runs = 40
+    result = app.tune(engine="skopt", method="bayes", runs=runs, seed=42, x=4.0)
+    assert len(result.func_vals) == runs
+    assert result.fun < -5.999
+    assert result.space.n_dims == 2
+    assert type(result.space[0][1]).__name__ == 'Integer'
+    assert type(result.space[1][1]).__name__ == 'Categorical'
+    for found, desired in zip(result.x, [1, 'abcdefghij']):
+        assert found == desired
+
+
+def test_skopt_tune_forest():
+    app = TuningTestApp()
+    runs = 30
+    result = app.tune(engine="skopt", method="forest", runs=runs, seed=42)
+    assert len(result.func_vals) == runs
+    assert result.fun < -9.96
+    assert result.space.n_dims == 3
+    assert type(result.space[0][1]).__name__ == 'Real'
+    assert type(result.space[1][1]).__name__ == 'Integer'
+    assert type(result.space[2][1]).__name__ == 'Categorical'
+    for found, desired in zip(result.x, [1.9370031589297412, 10, 'abcdefghij']):
+        if isinstance(found, float):
+            assert_allclose(found, desired)
+        else:
+            assert found == desired
+
+
+def test_skopt_tune_gradientboost():
+    app = TuningTestApp()
+    runs = 30
+    result = app.tune(engine="skopt", method="gradientboost", runs=runs, seed=42)
+    assert len(result.func_vals) == runs
+    assert result.fun < -9.97
+    assert result.space.n_dims == 3
+    assert type(result.space[0][1]).__name__ == 'Real'
+    assert type(result.space[1][1]).__name__ == 'Integer'
+    assert type(result.space[2][1]).__name__ == 'Categorical'
+    for found, desired in zip(result.x, [1.9535076154752264, 10, 'abcdefghij']):
+        if isinstance(found, float):
+            assert_allclose(found, desired)
+        else:
+            assert found == desired
+
+
+def test_get_optimizer():
     assert get_optimizer("bayes") == skopt.gp_minimize
     assert get_optimizer("gp") == skopt.gp_minimize
     assert get_optimizer("forest") == skopt.forest_minimize
